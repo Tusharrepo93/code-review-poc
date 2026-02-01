32f92b426da29da5c1226614b5c2eb0d643d216a
Tushar Batra <tusharcapricon@gmail.com>
2026-02-01 15:31:40 +0530

    Add calculator with some bugs

--- src/calculator.py
+++ src/calculator.py
@@ -5,14 +5,10 @@ def subtract(a, b):
     return a - b
 
 def divide(a, b):
-    # TODO: Add error handling
+    # TODO: This will crash if b is 0!
     return a / b
 
 def multiply(a, b):
     result = a * b
-    print("Result is: " + str(result))  # Bad practice: print in function
+    print("Result is: " + str(result))  # Bad practice
     return result
-
-# Missing docstrings
-# No input validation
-# Potential division by zero
